1d31f3935d6e1f11ff8d70aa6b47d60676a58780
Dialos <p.saltarin@outlook.it>
2025-04-14 17:09:46 +0200

    Refactor audio wave widget to include play/pause functionality and update UI layout

--- lib/features/home/view/widgets/audio_wave.dart
+++ lib/features/home/view/widgets/audio_wave.dart
@@ -1,5 +1,7 @@
 import 'package:audio_waveforms/audio_waveforms.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:musicapp_client/core/theme/color_palette.dart';
 
 class AudioWave extends StatefulWidget {
   final String path;
@@ -22,6 +24,16 @@ class _AudioWaveState extends State<AudioWave> {
     await playerController.preparePlayer(path: widget.path);
   }
 
+  Future<void> playAndPause() async {
+    if (!playerController.playerState.isPlaying) {
+      await playerController.startPlayer();
+      await playerController.setFinishMode(finishMode: FinishMode.stop);
+    } else if (!playerController.playerState.isPaused) {
+      await playerController.pausePlayer();
+    }
+    setState(() {});
+  }
+
   @override
   void dispose() {
     playerController.dispose();
@@ -30,7 +42,27 @@ class _AudioWaveState extends State<AudioWave> {
 
   @override
   Widget build(BuildContext context) {
-    return AudioFileWaveforms(
-        size: Size(double.infinity, 100), playerController: playerController);
+    return Row(
+      children: [
+        IconButton(
+          onPressed: playAndPause,
+          icon: Icon(playerController.playerState.isPlaying
+              ? CupertinoIcons.pause_solid
+              : CupertinoIcons.play_arrow_solid),
+        ),
+        Expanded(
+          child: AudioFileWaveforms(
+            size: Size(double.infinity, 100),
+            playerController: playerController,
+            playerWaveStyle: const PlayerWaveStyle(
+              fixedWaveColor: Palette.borderColor,
+              liveWaveColor: Palette.gradient2,
+              spacing: 6,
+              showSeekLine: false,
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
